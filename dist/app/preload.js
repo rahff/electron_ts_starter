704e03fd4f1ffa826c0597d3c760e4df0e4e665d
rahff <raphaelandrey99@gmail.com>
2022-11-02 20:06:22 +0100

    delete dist folder

--- dist/app/preload.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const electron_1 = require("electron");
-electron_1.contextBridge.exposeInMainWorld("electronApi", {
-    fileSystemAdapter: {
-        saveFile: (payload) => electron_1.ipcRenderer.invoke('file:save', payload)
-    }
-});
-//# sourceMappingURL=preload.js.map
